9b5b1f8eba388222ad777664e2b915e26b98fde3
Elliott Richmond <elliott@squareonemd.co.uk>
2023-07-30 16:06:53 +0100

    update

--- rotating-banner.php
+++ rotating-banner.php
@@ -1,11 +1,13 @@
 <?php
 /**
  * Plugin Name:       Rotating Banner
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       Rotating Banner is a simple block that can be rotated to any angle.
+ * Plugin URI:		  https://github.com/eirichmond/rotating-banner
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Elliott Richmond
+ * Author URI: 		  https://elliottrichmond.co.uk/
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       rotating-banner
